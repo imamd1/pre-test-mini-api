663d490d0364187b4ecd63de2fd918052583da8e
binokular <sakurakun002@gmail.com>
2022-08-10 12:50:27 +0700

    feat(init): first push to repository
    add project to github repository. Mini API already build in docker image
    
    Closes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pre-test-mini-api
+
+`git clone https://github.com/imamd1/pre-test-mini-api.git`
+`npm install`
+
+`npm start`
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
